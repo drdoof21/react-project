59286047367870f58d5068b884e2065797cfa9bf
drdoof21 <ilhamrdh21@gmail.com>
2021-08-08 00:38:00 +0700

    Creating pages and Todo component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
+import Todo from './Todo';
 
 const Todos = () => {
+    const todos = [
+        {
+            text: "Belajar React"
+        },
+        {
+            text: "Belajar props di react"
+        },
+        {
+            text: "Belajar React hooks"
+        },
+        {
+            text: "Belajar State"
+        },
+        {
+            text: "Belajar semuanya"
+        },
+        {
+            text: "Belajar React"
+        },
+        {
+            text: "Belajar props di react"
+        },
+        {
+            text: "Belajar React hooks"
+        },
+        {
+            text: "Belajar State"
+        },
+        {
+            text: "Belajar semuanya"
+        }
+    ]
+
     return(
         <section className="todos">
-            <div className="todo">
-                <span className="todo-text">Learning React</span>
-            </div>   
-            <div className="todo">
-                <span className="todo-text">Learning React</span>
-            </div> 
-            <div className="todo">
-                <span className="todo-text">Learning React</span>
-            </div> 
-            <div className="todo">
-                <span className="todo-text">Learning React</span>
-            </div> 
-            <div className="todo">
-                <span className="todo-text">Learning React</span>
-            </div> 
-            <div className="todo">
-                <span className="todo-text">Learning React</span>
-            </div> 
-            <div className="todo">
-                <span className="todo-text">Learning React</span>
-            </div> 
-            <div className="todo">
-                <span className="todo-text">Learning React</span>
-            </div> 
-            <div className="todo">
-                <span className="todo-text">Learning React</span>
-            </div> 
-            <div className="todo">
-                <span className="todo-text">Learning React</span>
-            </div>     
+            {todos.map(todo => {
+                return <Todo text ={todo.text} />
+            })}
         </section>
     )
 }
