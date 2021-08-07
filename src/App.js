ac45d9a752e1e327d410c7d96f391a6295a66475
drdoof21 <ilhamrdh21@gmail.com>
2021-08-08 00:13:52 +0700

    Creating Header, TodoForm, and Todos component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
 import React from 'react';
 import './style.css';
 import Paper from './components/Paper';
+import Header from './components/Header';
+import TodoForm from './components/TodoForm';
+import Todos from './components/Todos';
 
 function App() {
   return (
-    <div className="App">
       <Paper>
-            <section className="header">
-              <button className="header-btn main-black-color">Add!</button>
-              <h1 className="header-title">Todo list</h1>
-              <button className="header-btn main-red-color">Clear!</button>
-            </section>
-            <section className="add">
-              <form className="add-form">
-                <input type="text" className="add-input" />
-                <button className="add-btn main-black-color">Add</button>
-              </form>
-            </section>
-            <section className="todos">
-              <div className="todo">
-                <span className="todo-text">Learning React</span>
-              </div>   
-              <div className="todo">
-                <span className="todo-text">Learning React</span>
-              </div> 
-              <div className="todo">
-                <span className="todo-text">Learning React</span>
-              </div> 
-              <div className="todo">
-                <span className="todo-text">Learning React</span>
-              </div> 
-              <div className="todo">
-                <span className="todo-text">Learning React</span>
-              </div> 
-              <div className="todo">
-                <span className="todo-text">Learning React</span>
-              </div> 
-              <div className="todo">
-                <span className="todo-text">Learning React</span>
-              </div> 
-              <div className="todo">
-                <span className="todo-text">Learning React</span>
-              </div> 
-              <div className="todo">
-                <span className="todo-text">Learning React</span>
-              </div> 
-              <div className="todo">
-                <span className="todo-text">Learning React</span>
-              </div>      
-            </section>
+        <Header/>
+        <TodoForm/>
+        <Todos/>
       </Paper>
-    </div>
   );
 }
 
